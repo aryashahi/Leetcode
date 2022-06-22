106da9b95bffeff40ad564dbac4d38fbd0047743
ARYA SHAHI <71433950+aryashahi@users.noreply.github.com>
2022-06-22 15:34:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.91%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-       int i=0;
-       int j=0;
-        while(i<s.size() && j<t.size()){
+       int i=0,j=0;
+        
+        while(j<t.length()){
             if(s[i]==t[j])
             {
                 i++;
                 j++;
                 
+                
             }
             else
                 j++;
-           
-            
         }
-         if(i==s.size())
-                return true;
-        
-        return false;
-        
+        if(i<s.size())
+            return false;
+        else
+            return true;
     }
 };
