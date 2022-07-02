771b73333306793c4768740b75d77621b3d4f45f
ARYA SHAHI <71433950+aryashahi@users.noreply.github.com>
2022-07-02 13:50:13 +0530

    Time: 4 ms (34.13%), Space: 6.4 MB (42.91%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char>s1,t1;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='#'){
-                if(!s1.empty()) 
-                s1.pop(); 
-            }
-            else
-                s1.push(s[i]);
-              
-        }
-        for(int i=0;i<t.size();i++){
-            if(t[i]=='#' ){
-                if(!t1.empty()) 
-                t1.pop(); 
-            }
-            else
-                t1.push(t[i]);
-              
-        }
-        if(s1.size()!=t1.size())
-            return false;
-        while (s1.empty() == false) {
+        vector<char> s1,t1;
         
-        if (s1.top() == t1.top()) {
-            
-            s1.pop();
-            t1.pop();
+        
+        for(auto x:s)
+        {
+            if(x=='#')
+            { if(s1.size()!=0)
+            s1.pop_back();
         }
-        else {
-           return false;
+            else
+            s1.push_back(x);
         }
-    }
-        return true;
+        
+        for(auto x:t)
+        {
+            if(x=='#' )
+            {if(t1.size()!=0)
+            t1.pop_back();
+            }
+            else
+            t1.push_back(x);
+        }
+        return s1==t1;
+        
     }
 };
