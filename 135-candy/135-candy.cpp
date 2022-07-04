9a307441c357560eeb327d71e206f697dfaf3c64
ARYA SHAHI <71433950+aryashahi@users.noreply.github.com>
2022-07-05 00:13:34 +0530

    Time: 47 ms (21.19%), Space: 18.2 MB (23.59%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        if(n==1)
+            return 1;
+        
+        vector<int>cl(n,1);
+        vector<int>cr(n,1);
+        
+        
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                cl[i]=cl[i-1]+1;
+            
+        }
+        for(int i=n-2;i>=0;i--){
+
+        if(ratings[i]>ratings[i+1])
+                cr[i]=cr[i+1]+1;
+        }
+        
+        
+        long int ans=0;
+        
+        for(int i=0;i<n;i++){
+            ans+=max(cl[i],cr[i]);
+        }
+        return ans;
+    }
+};
